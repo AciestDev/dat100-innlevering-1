b00fc8a79709be99ade9ed5f69fa5157934c36eb
AciestDev <takle.nicholasrundgren@gmail.com>
2025-08-29 22:55:31 +0200

    TASK1 COMPLETED
    No math.round tho :(

--- Oppgave-1/OppgaveO1.java
+++ Oppgave-1/OppgaveO1.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 public class OppgaveO1 {
+
+//Boolean verdi som velger koden fra gjennomgått stoff + simplifisert eller improvisert kode på bunn    
+    private static final boolean ORIGINAL_QUESTION1 = true;
     public static void main(String[] args) {
         if (ORIGINAL_QUESTION1) {
             mainOriginal(args);
@@ -10,9 +13,13 @@ public class OppgaveO1 {
             mainImproved(args);
         }
     }
+
+// "Simplifisert" kode med else if statments istedenfor     
     public static void mainOriginal(String[] args) {
-// Every variable required for this code
+
+// Alle varablene som er nødvendig for denne koden
         int switchChooser = 0;
+        // Istedenfor å skrive 5 variabler A, B, C, osv legger jeg de inn i en array for å holde bedre kontroll på de
         double [] prosent = {1.7, 4.0, 13.7, 16.7, 17.7};
         double [] intekt = {217401, 306051, 697151, 942401, 1410751};
         double Skatt = 0;
@@ -78,8 +85,9 @@ public class OppgaveO1 {
                 break;   
         }
         in.close();
-    }
-// Dette er en forbedret kode    
+    } 
+    
+// Improved code, this will be in english as is standard practice for future jobs    
     public static void mainImproved(String[] args) {
 // Every variable required for this code
         double[] percent = {1.7, 4.0, 13.7, 16.7, 17.7};
@@ -93,18 +101,22 @@ public class OppgaveO1 {
         System.out.print("What is the gross income: ");
         double grossIncome = in.nextDouble();
 
+// Calculates tax and prints        
         for (int i = 0; i < bracketThresholdsL.size(); i++) {
             if (grossIncome < bracketThresholdsL.get(0)) {
                 System.out.println("You don't have to pay tax! ");
                 i = bracketThresholdsL.size();
             } else if(grossIncome > bracketThresholdsL.get(4)) {
                 tax = (percent[4] / 100) * grossIncome;
-                System.out.println("Your tax is2: " + tax);
+                System.out.println("Your tax is: " + tax);
                 i = bracketThresholdsL.size();
             } else if (grossIncome >= bracketThresholdsL.get(i) && grossIncome < bracketThresholdsL.get(i+1) && grossIncome > bracketThresholdsL.get(0) && grossIncome < bracketThresholdsL.get(4)) {
                 tax = (percent[i] / 100) * grossIncome;
-                System.out.println("Your tax isT: " + tax);
-            }
+                Math.round(tax);
+                System.out.println("Your tax is: " + tax);
+                i = bracketThresholdsL.size();
+
+            } 
         }    
         in.close();
     }
