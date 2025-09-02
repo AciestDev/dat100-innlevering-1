36fc93a282447d7f975ecc60c75724485d1fb94b
AciestDev <takle.nicholasrundgren@gmail.com>
2025-09-02 18:54:28 +0200

    fixing the code to be a little better

--- Oppgave-1/OppgaveO1.java
+++ Oppgave-1/OppgaveO1.java
@@ -18,17 +18,13 @@ public class OppgaveO1 {
     public static void mainOriginal(String[] args) {
 
 // Alle varablene som er nødvendig for denne koden
-        int switchChooser = 0;
-        // Istedenfor å skrive 5 variabler A, B, C, osv legger jeg de inn i en array for å holde bedre kontroll på de
+// Istedenfor å skrive 5 variabler A, B, C, osv legger jeg de inn i en array for mer oversikt
         double [] prosent = {1.7, 4.0, 13.7, 16.7, 17.7};
         double [] intekt = {217401, 306051, 697151, 942401, 1410751};
-        double Skatt = 0;
-        double trinn1FaktorProsent = prosent[0] / 100;
-        double trinn2FaktorProsent = prosent[1] / 100;
-        double trinn3FaktorProsent = prosent[2] / 100;
-        double trinn4FaktorProsent = prosent[3] / 100;
-        double trinn5FaktorProsent = prosent[4] / 100;
-        double [] faktorProsent = {trinn1FaktorProsent, trinn2FaktorProsent, trinn3FaktorProsent, trinn4FaktorProsent, trinn5FaktorProsent};
+        double skatt = 0;
+
+// Primative kode med før beregnet intekt per trinn fra øverst til minst
+        double [] intektPTrinn = {78214.45, 33599.25, 15644, 1507.05};
 // Input code
         Scanner in = new Scanner(System.in);
         System.out.print("Hva er bruttolønnen: ");
@@ -36,53 +32,43 @@ public class OppgaveO1 {
 
 // Calculating the tax
         if  (bruttoLonn < intekt[0] && bruttoLonn > 0) {
-            switchChooser = 1;
+            System.out.println("Du betaler ingen trinnskatt! ");
 
-        } else if (bruttoLonn >= intekt[0] && bruttoLonn < intekt[1]) {
-            Skatt = bruttoLonn * faktorProsent[0];
-            Skatt = Math.round(Skatt);
-            switchChooser = 2;
-
-        } else if (bruttoLonn >= intekt[1] && bruttoLonn < intekt[2]) {
-            Skatt = bruttoLonn * faktorProsent[1];
-            Skatt = Math.round(Skatt);
-            switchChooser = 3;
-        } else if (bruttoLonn >= intekt[2] && bruttoLonn < intekt[3]) {
-            Skatt = bruttoLonn * faktorProsent[2];
-            Skatt = Math.round(Skatt);
-            switchChooser = 4;
-        } else if(bruttoLonn >= intekt[3] && bruttoLonn < intekt[4]) {
-            Skatt = bruttoLonn * faktorProsent[3];
-            Skatt = Math.round(Skatt);
-            switchChooser = 5;
         } else if (bruttoLonn >= intekt[4]) {
-            Skatt = bruttoLonn * faktorProsent[4];
-            Skatt = Math.round(Skatt);
-            switchChooser = 6;
-        } else {
-            System.out.println("Det er ikke valid svar");
-        }
-
-// Printing how much
-        switch(switchChooser) {
-            case 1:
-                System.out.println("Du betaler ingen trinnskatt! ");
-                break;
-            case 2:
-                System.out.println("Du betaler " + prosent[0] + "% trinnskatt som utgjør: " + Skatt +"kr");
-                break;
-            case 3:
-                System.out.println("Du betaler " + prosent[1] + "% trinnskatt som utgjør: " + Skatt + "kr");
-                break;
-            case 4:
-                System.out.println("Du betaler " + prosent[2] + "% trinnskatt som utgjør: " + Skatt + "kr");
-                break;
-            case 5:
-                System.out.println("Du betaler " + prosent[3] + "% trinnskatt som utgjør: " + Skatt + "kr");
-                break;
-            case 6:
-                System.out.println("Du betaler " + prosent[4] + "% trinnskatt som utgjør: " + Skatt + "kr");
-                break;   
+            bruttoLonn = bruttoLonn - intekt[4];
+            skatt = bruttoLonn * (prosent[4] / 100);
+            skatt = skatt + intektPTrinn[0];
+            skatt = skatt + intektPTrinn[1];
+            skatt = skatt + intektPTrinn[2];
+            skatt = skatt + intektPTrinn[3];
+            skatt = Math.round(skatt);
+            System.out.println("Du betaler: " + skatt +"kr i skatt");
+        } else if (bruttoLonn >= intekt[3]) {
+            bruttoLonn = bruttoLonn - intekt[3];
+            skatt = bruttoLonn * (prosent[3] / 100);
+            skatt = skatt + intektPTrinn[1];
+            skatt = skatt + intektPTrinn[2];
+            skatt = skatt + intektPTrinn[3];
+            skatt = Math.round(skatt);
+            System.out.println("Du betaler: " + skatt +"kr i skatt");
+        } else if (bruttoLonn >= intekt[2]) {
+            bruttoLonn = bruttoLonn - intekt[2];
+            skatt = bruttoLonn * (prosent[2] / 100);
+            skatt = skatt + intektPTrinn[2];
+            skatt = skatt + intektPTrinn[3];
+            skatt = Math.round(skatt);
+            System.out.println("Du betaler: " + skatt +"kr i skatt");
+        } else if (bruttoLonn >= intekt[1]) {
+            bruttoLonn = bruttoLonn - intekt[1];
+            skatt = bruttoLonn * (prosent[1] / 100);
+            skatt = skatt + intektPTrinn[3];
+            skatt = Math.round(skatt);
+            System.out.println("Du betaler: " + skatt +"kr i skatt");
+        } else if (bruttoLonn >= intekt[0]) {
+            bruttoLonn = bruttoLonn - intekt[0];
+            skatt = bruttoLonn * (prosent[0] / 100);
+            skatt = Math.round(skatt);
+            System.out.println("Du betaler: " + skatt +"kr i skatt");
         }
         in.close();
     } 
